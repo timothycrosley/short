dcf25ec01b5406d9e0a35ac7dd8112239c0912a5
Timothy Crosley <timothy.crosley@gmail.com>
2017-10-04 17:40:22 -0700

    Add accessor support, using *

--- short/compile.py
+++ short/compile.py
@@ -22,7 +22,7 @@
 """
 import re
 
-MAGIC_ATTRIBUTES = {'#': 'id', '@': 'href', '$': 'name', ',': 'class', '!': 'src'}
+MAGIC_ATTRIBUTES = {'#': 'id', '@': 'href', '$': 'name', ',': 'class', '!': 'src', '*': 'accessor'}
 STRAIGHT_MAGIC = ''.join(MAGIC_ATTRIBUTES.keys())
 ESCAPED_MAGIC = ''.join(re.escape(magic) for magic in MAGIC_ATTRIBUTES.keys())
 QUOTED__text = r"""(?:(?:'(?:\\'|[^'])*')|(?:"(?:\\"|[^"])*"))"""
--- tests/test_short.py
+++ tests/test_short.py
@@ -6,12 +6,13 @@ import short
 
 def test_short():
     """Test to ensure short works as expected"""
-    compiled = short.compile.text('> ,hey$there@is#something!more')
+    compiled = short.compile.text('> ,hey$there@is#something!more*bacon')
     assert 'class="hey"' in compiled
     assert 'name="there"' in compiled
     assert 'href="is"' in compiled
     assert 'id="something"' in compiled
     assert 'src="more"' in compiled
+    assert 'accessor="bacon"' in compiled
 
     assert short.compile.text(',here\n\tnor there') == '<div class="here">\n\tnor there\n</div>\n'
     assert short.compile.text('$here\n\tnor there') == '<div name="here">\n\tnor there\n</div>\n'
